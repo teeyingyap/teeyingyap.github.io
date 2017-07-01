f9052e4c314078880cec8bdb6837473ff1191df6
teeyingyap <yapteeying@gmail.com>
2017-07-02 00:12:55 +0800

    added js file

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	var a = document.getElementById("home-name");
+	var b = document.getElementById("home-label");
+     a.className += " loaded";
+     b.className += " loaded";
+}
