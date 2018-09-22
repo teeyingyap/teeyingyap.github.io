9de90224c043bc21f8461a70f15f492b0160d577
teeyingyap <yapteeying@gmail.com>
2018-09-23 01:51:17 +0800

    added media queries

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	var a = document.getElementById("home-name");
-	var b = document.getElementById("home-label");
-     a.className += " loaded";
-     b.className += " loaded";
-}
---
+++ js/index.js
@@ -0,0 +1,57 @@
+var elements = document.getElementsByClassName('about-animate');
+var triggered = false;
+var TitleAnimate = function(el) {
+  this.el = el;
+  this.txt = '';
+  this.tick();
+};
+
+TitleAnimate.prototype.tick = function() {
+  var fullTxt = 'Hi, I\'m Tee Ying.';
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+  
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+
+
+var waypoint = new Waypoint({
+  element: elements[0],
+  handler: function(direction) {
+    if (!triggered) {
+      // do animation
+      new TitleAnimate(elements[0]);
+      // add cursor
+      var css = document.createElement("style");
+      css.type = "text/css";
+      css.innerHTML = ".about-animate > .wrap { border-right: 0.08em solid #666 }";
+      document.body.appendChild(css);
+    }
+    triggered = true;
+    
+  },
+  offset: 'bottom-in-view'
+})
+
+
+
+
+window.onload = function() {
+	var a = document.getElementById("home-name");
+	var b = document.getElementById("home-label");
+	var c = document.getElementById("links");
+
+    a.className += " loaded";
+    b.className += " loaded";
+    c.className += " loaded";
+
+  	
+}
+
